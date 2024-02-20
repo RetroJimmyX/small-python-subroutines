c3f68dcd6c2ee4791185394c1c8d229af70e6681
RetroJimmyX <51858595+RetroJimmyX@users.noreply.github.com>
2024-02-20 18:44:12 +0000

    Create Counting Characters in a String.py

---
+++ Counting Characters in a String.py
@@ -0,0 +1,22 @@
+# My first function
+def method1(s):
+    letters = {}
+    for i in range(len(s)):
+        if s[i] in letters:
+            letters[s[i]] += 1
+        else:
+            letters[s[i]] = 1
+    return letters
+
+# My improved function
+def method2(s):
+    return {char:s.count(char) for char in s}
+
+# A pro coder's solution
+from collections import Counter as method3
+
+s= "eFwSdVEImDaGeovGahugQeOKauADHIISUcLprrhAWgXqpvUCYlQNmcQDagulYECtqceMdYeZyueuOeWNsZdptWEfvZTe"
+
+print("Method 1:",method1(s))
+print("Method 2:",method2(s))
+print("Method 3:",method3(s))

75d8c1a25edaaa3f827cabf1462806284f680634
RetroJimmyX <51858595+RetroJimmyX@users.noreply.github.com>
2024-02-23 21:36:29 +0000

    Update Check Valid Coupon Code and Date.py

--- Check Valid Coupon Code and Date.py
+++ Check Valid Coupon Code and Date.py
@@ -1,5 +1,7 @@
 from datetime import datetime as dt
 def check_coupon(ec, cc, cd, ed):
+    ''' Returns boolean for entered code, correct code, current dat, expired date
+    where dates are in the format "September 15, 1951"
     return ec and ec == cc and dt.strptime(cd,"%B %d, %Y") <= dt.strptime(ed,"%B %d, %Y") 
 
 # https://www.codewars.com/kata/539de388a540db7fec000642
